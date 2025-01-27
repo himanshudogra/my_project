a46e8f3dd8ea1cb5bc88766272f384c4e9b64260
hdogra <himanshudogra96@gmail.com>
2025-01-27 15:29:07 +0530

    Initial commit with Python project and GitHub Actions workflow

---
+++ main.py
@@ -0,0 +1,19 @@
+# main.py
+
+def add(a, b):
+    """
+    Function to add two numbers.
+    """
+    return a + b
+
+def subtract(a, b):
+    """
+    Function to subtract two numbers.
+    """
+    return a - b
+
+if __name__ == "__main__":
+    # This is just a simple way to use the functions if the script is run directly
+    print("Addition of 1 and 2 is:", add(1, 2))
+    print("Subtraction of 3 and 1 is:", subtract(3, 1))
+
