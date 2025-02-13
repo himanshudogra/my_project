e7be8fe0affefd0dacfe726b19e809ac434307de
himanshudogra <himanshudogra96@gmail.com>
2025-02-13 08:46:56 +0530

    Fix ModuleNotFoundError in test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the project's root directory to sys.path
+sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+
 from main import add
 
 def test_add():
